ad3f6ae4a9b1b8061684ec548f6357db7be99662
CHOHEE2000 <ljdyjv@naver.com>
2023-05-24 11:06:14 +0900

    w3

---
+++ cs/src/cs_week03/N_1052.java
@@ -0,0 +1,10 @@
+package cs_week03;
+
+public class N_1052 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ cs/src/cs_week03/N_2178.java
@@ -0,0 +1,10 @@
+package cs_week03;
+
+public class N_2178 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
